2fd082fb50993d9736f9e467614988f22320e8d3
Jayed <jayedf@nycstudents.net>
2025-11-17 19:37:48 -0500

    Finished

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
     FrogSimulation f = new FrogSimulation(24, 5);
       System.out.println(f.runSimulations(400));
+      System.out.println(f.simulate());
+      System.out.println(f.hopDistance());
 
     }
 }
